65726fd7033e73dd013022164e82581e41666551
Luis Muñoz <luismunoz@cpan.org>
2004-02-17 11:52:15 +0000

    Import of LUISMUNOZ/Crypt-PasswdMD5-1.3 from CPAN.
    gitpan-cpan-distribution: Crypt-PasswdMD5
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         LUISMUNOZ/Crypt-PasswdMD5-1.3.tar.gz
    gitpan-cpan-author:       LUISMUNOZ
    gitpan-cpan-maturity:     released

--- PasswdMD5.pm
+++ PasswdMD5.pm
@@ -14,17 +14,12 @@
 # this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
 # ----------------------------------------------------------------------------
 #
-# 19980710 lem@cantv.net: Initial release
-# 19990402 bryan@eai.com: Added apache_md5_crypt to create a valid hash
-#                         for use in .htpasswd files
-# 20001006 wrowe@lnd.com: Requested apache_md5_crypt to be
-#			  exported by default.
-# 20010706 lem@cantv.net: Use Digest::MD5 instead of the (obsolete) MD5.
+# $Id: PasswdMD5.pm,v 1.3 2004/02/17 11:21:38 lem Exp $
 #
 ################
 
 package Crypt::PasswdMD5;
-$VERSION='1.2';
+$VERSION='1.3';
 require 5.000;
 require Exporter;
 @ISA = qw(Exporter);
@@ -32,13 +27,15 @@ require Exporter;
 
 =head1 NAME
 
-unix_md5_crypt - Provides interoperable MD5-based crypt() function
+Crypt::PasswdMD5 - Provides interoperable MD5-based crypt() functions
 
 =head1 SYNOPSIS
 
-	use Crypt::PasswdMD5;
+    use Crypt::PasswdMD5;
 
-	$cryptedpassword = unix_md5_crypt($password, $salt);
+    $cryptedpassword = unix_md5_crypt($password, $salt);
+    $apachepassword = apache_md5_crypt($password, $salt);
+
 
 =head1 DESCRIPTION
 
@@ -57,9 +54,12 @@ C<.htpasswd> files. This was contributed by Bryan Hart <bryan@eai.com>.
 As suggested by William A. Rowe, Jr. <wrowe@lnd.com>, it is 
 exported by default.
 
+For both functions, if a salt value is not supplied, a random salt will be
+generated.  Contributed by John Peacock <jpeacock@cpan.org>.
+
 =cut
 
-$Magic = '$1$';			# Magic string
+$Magic = q/$1$/;			# Magic string
 $itoa64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 use Digest::MD5;
@@ -76,7 +76,7 @@ sub to64 {
 
 sub apache_md5_crypt {
 	# change the Magic string to match the one used by Apache
-	local $Magic = '$apr1$';
+	local $Magic = q/$apr1$/;
 	
 	unix_md5_crypt(@_);
 }
@@ -85,11 +85,19 @@ sub unix_md5_crypt {
     my($pw, $salt) = @_;
     my $passwd;
 
-    $salt =~ s/^\Q$Magic//;	# Take care of the magic string if
+    if ( defined $salt ) {
+
+	$salt =~ s/^\Q$Magic//;	# Take care of the magic string if
 				# if present.
 
-    $salt =~ s/^(.*)\$.*$/$1/;	# Salt can have up to 8 chars...
-    $salt = substr($salt, 0, 8);
+	$salt =~ s/^(.*)\$.*$/$1/;	# Salt can have up to 8 chars...
+	$salt = substr($salt, 0, 8);
+    }
+    else {
+	$salt = '';	 	# in case no salt was proffered
+	$salt .= substr($itoa64,int(rand(64)+1),1)
+			while length($salt) < 8;
+    }
 
     $ctx = new Digest::MD5;		# Here we start the calculation
     $ctx->add($pw);		# Original password...
@@ -153,10 +161,60 @@ sub unix_md5_crypt {
     $passwd .= to64(int(unpack("C", substr($final, 11, 1))), 2);
 
     $final = '';
-    $Magic . $salt . '$' . $passwd;
+    $Magic . $salt . q/$/ . $passwd;
 }
 
 1;
 
+__END__
+
+=pod
+
+=head2 EXPORT
+
+None by default.
 
 
+=head1 HISTORY
+
+$Id: PasswdMD5.pm,v 1.3 2004/02/17 11:21:38 lem Exp $
+
+ 19980710 luismunoz@cpan.org: Initial release
+ 19990402 bryan@eai.com: Added apache_md5_crypt to create a valid hash
+                        for use in .htpasswd files
+ 20001006 wrowe@lnd.com: Requested apache_md5_crypt to be
+			exported by default.
+ 20010706 luismunoz@cpan.org: Use Digest::MD5 instead of the (obsolete) MD5.
+
+$Log: PasswdMD5.pm,v $
+Revision 1.3  2004/02/17 11:21:38  lem
+Modified the POD so that ABSTRACT can work
+Added usage example for apache_md5_crypt()
+
+Revision 1.2  2004/02/17 11:04:35  lem
+Added patch for random salts from John Peacock (Thanks John!)
+De-MS-DOS-ified the file
+Replaced some '' with q// to make Emacs color highlighting happy
+Added CVS docs
+Completed the missing sections of the POD documentation
+Changed my email address to the Perl-related one for consistency
+The file is now encoded in ISO-8859-1
+
+
+=head1 LICENSE AND WARRANTY
+
+This code and all accompanying software comes with NO WARRANTY. You
+use it at your own risk.
+
+This code and all accompanying software can be used freely under the
+same terms as Perl itself.
+
+=head1 AUTHOR
+
+Luis E. Muñoz <luismunoz@cpan.org>
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
